a8d18294c47c93ecdbfec97635b3a6d0d493582c
Steve Lau <stevelauc@outlook.com>
2022-11-07 17:29:45 +0800

    fmt: fix format check

--- src/backend/mod.rs
+++ src/backend/mod.rs
@@ -1,6 +1,6 @@
 //! Backend for our fs implementation
 //!
-//! Our backend is divided into two parts: 
+//! Our backend is divided into two parts:
 //! 1. libc-like syscalls.
 //! 2. Rusty encapsulations for those libc-like syscalls.
 
